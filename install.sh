683b6b2432546da6be20926aad0ef4c013d3f05a
Gabriel Ramírez <geramirez@users.noreply.github.com>
2021-08-12 05:17:11 +0000

    removing installs

---
+++ install.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+export USERNAME=`whoami`
+
+exec > >(tee -i $HOME/dotfiles_install.log)
+exec 2>&1
+set -x
+
+ln -s $(pwd)/tmux.conf $HOME/.tmux.conf
+ln -s $(pwd)/vimrc $HOME/.vimrc
+ln -s $(pwd)/vim $HOME/.vim
+# ln -s $(pwd)/emacs $HOME/.emacs
+# ln -s $(pwd)/screenrc $HOME/.screenrc
+vim -Es -u $HOME/.vimrc -c "PlugInstall | qa"
+
+# zsh configuration
+sudo apt-get update
+sudo apt-get install -y zsh fonts-powerline
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+sh -c "$(curl -fsSL https://starship.rs/install.sh)" -- -y
+echo "$(starship init zsh)" >> ~/.zshrc
