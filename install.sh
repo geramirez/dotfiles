14b1965c6675770bd04c67fc952c3f66ff67829d
Gabriel Ramírez <geramirez@users.noreply.github.com>
2021-10-01 11:48:33 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,26 +5,29 @@ exec > >(tee -i $HOME/dotfiles_install.log)
 exec 2>&1
 set -x
 
+#cs_tooling
+ln -s $(pwd)/cs_tooling $HOME/.cs_tooling
+
+sudo apt-get install -y zsh fonts-powerline neovim rcm tmux exuberant-ctags silversearcher-ag
+rcup -f -v -d . -t linux -t development
+
 unset GIT_EDITOR
 git config --global core.editor vim
 git config --global push.default simple
 git config --global commit.gpsign true
 git config --global commit.gpgsign true
 
-mkdir -p ~/.config/nvim/
-ln -s $(pwd)/init.vim $HOME/.config/nvim/init.vim
 ln -s $(pwd)/tmux.conf $HOME/.tmux.conf
 ln -s $(pwd)/vimrc $HOME/.vimrc
 ln -s $(pwd)/vim $HOME/.vim
-# ln -s $(pwd)/emacs $HOME/.emacs
-# ln -s $(pwd)/screenrc $HOME/.screenrc
+ln -s $(pwd)/screenrc $HOME/.screenrc
 vim -Es -u $HOME/.vimrc -c "PlugInstall | qa"
 
 # zsh configuration
-sudo apt-get update
-sudo apt-get install -y zsh fonts-powerline neovim
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-sh -c "$(curl -fsSL https://starship.rs/install.sh)" -- -y
-echo "$(starship init zsh)" >> ~/.zshrc
+sudo chsh -s "$(which zsh)" "$(whoami)"
 
-ln -s $(pwd)/cs_tooling $HOME/.cs_tooling
+
+if [[ -x /workspaces/github/bin/build-ctags ]]; then
+  /workspaces/github/bin/build-ctags
+fi
