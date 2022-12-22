e5e910725c4af5192b6c1c705fb23f97f3e3b7f0
Gabriel Ramírez <geramirez@users.noreply.github.com>
2022-12-22 11:34:36 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,10 +5,13 @@ exec > >(tee -i $HOME/dotfiles_install.log)
 exec 2>&1
 set -x
 
-sudo apt update
-sudo apt install -y fish tmux neovim
+sudo add-apt-repository ppa:neovim-ppa/stable
+sudo apt-get update
+sudo apt install -y fish tmux python-dev python-pip python3-dev python3-pip neovim
 
 ln -s $(pwd)/tmux.conf $HOME/.tmux.conf
 
 mkdir -p /home/codespace/.config/nvim/
 mv $(pwd)/init.vim $HOME/.config/nvim/init.vim
+
+nvim +'PlugInstall --sync' +qa
